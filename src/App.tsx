bab47fc4ab9964d0f5f848c36cceee70d1d770d5
Acubyte <sivut_mek@outlook.com>
2023-10-07 04:57:23 +0700

    setup react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Work from "./Work";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
-  return <Work />;
+  
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Work />
+    </QueryClientProvider>
+  )
 }
 
 export default App;
