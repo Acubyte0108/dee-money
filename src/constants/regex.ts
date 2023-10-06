5566ce1d3119b6f55b4c2952f14deca403ff442c
Acubyte <sivut_mek@outlook.com>
2023-10-07 01:47:14 +0700

    adjust regex

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -3,8 +3,8 @@ const regex = {
   englishCharacterOnly: /^[A-Za-z\s'.-]*$/,
   /* Matches the name word that fisrt letter must be capitalized whether it come after spacebar and that word must not start with apostrophe, dot and hyphen symbol. */
   capitalFirst: /^(\s*(?:[A-Z][a-z'.-]*)\s*)+$/,
-  /* Matches the name word that contains one apostrophe and/or one dot and/or hyphen symbol. */
-  containsOneSymbol: /^(?!.*[.].*[.])(?!.*['].*['])(?!.*[-].*[-])[A-Za-z\s]*[.'.\\-]?[A-Za-z\s]*$/,
+  /* Matches the name word that contains only one apostrophe and one dot and hyphen symbol. */
+  containsOneSymbol: /^(?!.*(['.-]).*\1)[\w'. -]*$/,
   /* Matches whitespace at the beginning, in between and more than one, or at the end of a string. */
   specialWhitespace: /^\s|\s\s+|\s$/,
 };
