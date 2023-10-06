764637921f869d43c66775497e0ab8f8b4c20764
Acubyte <sivut_mek@outlook.com>
2023-10-07 01:19:48 +0700

    adjust regex

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,10 +1,10 @@
 const regex = {
-  /* Matches the name that contains only English alphabets and also accepts apostrophe symbol and dot symbol. */
-  englishCharacterOnly: /^[A-Za-z\s'.]*$/,
-  /* Matches the name word that fisrt letter must be capitalized whether it come after spacebar and that word must not start with apostrophe symbol and dot symbol. */
-  capitalFirst: /^(\s*(?:[A-Z][a-z'.]*)\s*)+$/,
-  /* Matches the name word that contains one apostrophe symbol and/or one dot symbol. */
-  containsOneSymbol: /^(?!.*[.].*[.])[A-Za-z\s]*[.']?[A-Za-z\s]*$/,
+  /* Matches the name that contains only English alphabets and also accepts apostrophe, dot and hyphen symbol. */
+  englishCharacterOnly: /^[A-Za-z\s'.-]*$/,
+  /* Matches the name word that fisrt letter must be capitalized whether it come after spacebar and that word must not start with apostrophe, dot and hyphen symbol. */
+  capitalFirst: /^(\s*(?:[A-Z][a-z'.-]*)\s*)+$/,
+  /* Matches the name word that contains one apostrophe and/or one dot and/or hyphen symbol. */
+  containsOneSymbol: /^(?!.*[.].*[.])(?!.*['].*['])(?!.*[-].*[-])[A-Za-z\s]*[.'.\\-]?[A-Za-z\s]*$/,
   /* Matches whitespace at the beginning, in between and more than one, or at the end of a string. */
   specialWhitespace: /^\s|\s\s+|\s$/,
 };
