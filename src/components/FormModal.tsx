f47c282010ab2679d534ee9d371da6edd8f622d0
Acubyte <sivut_mek@outlook.com>
2023-10-09 21:16:37 +0700

    adjust input type

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -224,7 +224,7 @@ const FormModal = (props: FormModalProps) => {
                   <input
                     {...register("email")}
                     id="email"
-                    type="email"
+                    type="text"
                     autoComplete="off"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
