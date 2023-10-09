74f8c6ba8dce1bb9fc6cc63f3aba49f6eb19170f
Acubyte <sivut_mek@outlook.com>
2023-10-10 02:57:41 +0700

    add test suite for CustomersTable

---
+++ src/__tests__/CustomersTable.test.tsx
@@ -0,0 +1,60 @@
+import { render, fireEvent } from "@testing-library/react";
+import CustomersTable from "../components/CustomersTable";
+import { Customer } from "../components/Work";
+import { useMediaQuery } from 'react-responsive';
+
+jest.mock('react-responsive', () => ({
+  useMediaQuery: jest.fn(),
+}));
+
+describe('Test CustomersTable component', () => {
+  const mockHandleEditUser = jest.fn();
+  const mockHandleDeleteUser = jest.fn();
+
+  const customers: Customer[] = [
+    {
+        id: "1",
+        firstName: "TestOne",
+        lastName: "Test-LastOne",
+        title: "Corporate Tactics Engineer",
+        country: "Malta",
+        email: "test1.last1@example.com",
+      },
+      {
+        id: "2",
+        firstName: "TestTwo",
+        lastName: "Test-LastTwo",
+        title: "Corporate Tactics Engineer",
+        country: "Malta",
+        email: "test2.last2@example.com",
+      },
+  ];
+
+  beforeEach(() => {
+    (useMediaQuery as jest.Mock).mockImplementation((query) => {
+      if (query.query === '(min-width: 640px)') {
+        return true;
+      }
+      if (query.query === '(max-width: 639px)') {
+        return false;
+      }
+    });
+  });
+
+  it('renders without crashing', () => {
+    render(<CustomersTable customers={customers} handleEditUser={mockHandleEditUser} handleDeleteUser={mockHandleDeleteUser} />);
+  });
+
+  it('calls handleEditUser and handleDeleteUser when respective buttons are clicked', () => {
+    const { getAllByTestId } = render(<CustomersTable customers={customers} handleEditUser={mockHandleEditUser} handleDeleteUser={mockHandleDeleteUser} />);
+    
+    const editButtons = getAllByTestId('edit-button');
+    const deleteButtons = getAllByTestId('delete-button');
+
+    fireEvent.click(editButtons[0]);
+    expect(mockHandleEditUser).toHaveBeenCalledWith(customers[0]);
+
+    fireEvent.click(deleteButtons[0]);
+    expect(mockHandleDeleteUser).toHaveBeenCalledWith(customers[0]);
+  });
+});
