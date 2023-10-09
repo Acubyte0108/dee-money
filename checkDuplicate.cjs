7ce3e3407fbea25e44097a67884090a3c0cb227c
Acubyte <sivut_mek@outlook.com>
2023-10-10 05:33:17 +0700

    adjust logic in checkDuplicate middleware

--- checkDuplicate.cjs
+++ checkDuplicate.cjs
@@ -1,22 +1,36 @@
 const fs = require("fs");
 
 module.exports = async (req, res, next) => {
-  if (req.method === 'POST' && req.originalUrl === '/customers') {
-    const newCustomer = req.body;
-  
-    const data = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
-    const customers = data.customers;
+  const newCustomer = req.body;
 
+  const data = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
+  const customers = data.customers;
+
+  if (req.method === 'POST' && req.originalUrl.includes('/customers')) {
     const isDuplicate = customers.some(customer => 
-      customer.firstName === newCustomer.firstName &&
-      customer.lastName === newCustomer.lastName &&
+      (customer.firstName === newCustomer.firstName &&
+      customer.lastName === newCustomer.lastName) ||
       customer.email === newCustomer.email
     );
 
     if (isDuplicate) {
-      return res.status(400).json({ message: 'Duplicate customer' });
+      return res.status(400).json({ message: '[POST] Duplicate customer' });
+    }
+  } else if (req.method === 'PATCH' && req.originalUrl.includes('/customers')) {
+    const customerId = req.originalUrl.split('/').pop();
+
+    const isDuplicate = customers.some(customer => 
+      customer.id !== customerId && (
+        (customer.firstName === newCustomer.firstName &&
+        customer.lastName === newCustomer.lastName) ||
+        customer.email === newCustomer.email
+      )
+    );
+
+    if (isDuplicate) {
+      return res.status(400).json({ message: '[PATCH] Duplicate customer' });
     }
   }
   
   next();
-}
+};
