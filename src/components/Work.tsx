438c98fcefc2af28b83579df862c3fc8a8803554
Acubyte <sivut_mek@outlook.com>
2023-10-09 18:39:23 +0700

    adjust placeholder

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -115,7 +115,7 @@ const Example = () => {
           <input 
             id="search-customers"
             className="block w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="Search... ex. customers name, email, etc."
+            placeholder="Search... ex. customer name, email, etc."
             autoComplete="off"
             value={text}
             onChange={(e) => setText(e.target.value)}
