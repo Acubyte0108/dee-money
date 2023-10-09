4e9d414081dc52da9bdd496b1601fec16b7fa4d3
Acubyte <sivut_mek@outlook.com>
2023-10-10 03:27:38 +0700

    format code

--- src/__tests__/CustomersTable.test.tsx
+++ src/__tests__/CustomersTable.test.tsx
@@ -1,60 +1,76 @@
 import { render, fireEvent } from "@testing-library/react";
 import CustomersTable from "../components/CustomersTable";
+import { useMediaQuery } from "react-responsive";
 import { Customer } from "../components/Work";
-import { useMediaQuery } from 'react-responsive';
 
-jest.mock('react-responsive', () => ({
+jest.mock("react-responsive", () => ({
   useMediaQuery: jest.fn(),
 }));
 
-describe('Test CustomersTable component', () => {
-  const mockHandleEditUser = jest.fn();
-  const mockHandleDeleteUser = jest.fn();
+beforeEach(() => {
+  (useMediaQuery as jest.Mock).mockImplementation((query) => {
+    if (query.query === "(min-width: 640px)") {
+      return true;
+    }
+    if (query.query === "(max-width: 639px)") {
+      return false;
+    }
+  });
+});
 
-  const customers: Customer[] = [
-    {
-        id: "1",
-        firstName: "TestOne",
-        lastName: "Test-LastOne",
-        title: "Corporate Tactics Engineer",
-        email: "test1.last1@example.com",
-        country: "Malta"       
-      },
-      {
-        id: "2",
-        firstName: "TestTwo",
-        lastName: "Test-LastTwo",
-        title: "Corporate Tactics Engineer",
-        email: "test2.last2@example.com",
-        country: "Malta"
-      },
-  ];
+afterEach(() => {
+    jest.clearAllMocks();
+});
 
-  beforeEach(() => {
-    (useMediaQuery as jest.Mock).mockImplementation((query) => {
-      if (query.query === '(min-width: 640px)') {
-        return true;
-      }
-      if (query.query === '(max-width: 639px)') {
-        return false;
-      }
-    });
+const mockHandleEditUser = jest.fn();
+const mockHandleDeleteUser = jest.fn();
+
+const mockCustomers: Customer[] = [
+  {
+    id: "1",
+    firstName: "TestOne",
+    lastName: "Test-LastOne",
+    title: "Corporate Tactics Engineer",
+    email: "test1.last1@example.com",
+    country: "Malta",
+  },
+  {
+    id: "2",
+    firstName: "TestTwo",
+    lastName: "Test-LastTwo",
+    title: "Corporate Tactics Engineer",
+    email: "test2.last2@example.com",
+    country: "Malta",
+  },
+];
+
+describe("Test CustomersTable component", () => {
+  it("renders without crashing", () => {
+    render(
+      <CustomersTable
+        customers={mockCustomers}
+        handleEditUser={mockHandleEditUser}
+        handleDeleteUser={mockHandleDeleteUser}
+      />
+    );
   });
 
-  it('renders without crashing', () => {
-    render(<CustomersTable customers={customers} handleEditUser={mockHandleEditUser} handleDeleteUser={mockHandleDeleteUser} />);
-  });
+  it("calls handleEditUser and handleDeleteUser when respective buttons are clicked", () => {
+    const { getAllByTestId } = render(
+      <CustomersTable
+        customers={mockCustomers}
+        handleEditUser={mockHandleEditUser}
+        handleDeleteUser={mockHandleDeleteUser}
+      />
+    );
 
-  it('calls handleEditUser and handleDeleteUser when respective buttons are clicked', () => {
-    const { getAllByTestId } = render(<CustomersTable customers={customers} handleEditUser={mockHandleEditUser} handleDeleteUser={mockHandleDeleteUser} />);
-    
-    const editButtons = getAllByTestId('edit-button');
-    const deleteButtons = getAllByTestId('delete-button');
+    const editButtons = getAllByTestId("edit-button");
+    const deleteButtons = getAllByTestId("delete-button");
 
     fireEvent.click(editButtons[0]);
-    expect(mockHandleEditUser).toHaveBeenCalledWith(customers[0]);
+    expect(mockHandleEditUser).toHaveBeenCalledWith(mockCustomers[0]);
 
     fireEvent.click(deleteButtons[0]);
-    expect(mockHandleDeleteUser).toHaveBeenCalledWith(customers[0]);
+    expect(mockHandleDeleteUser).toHaveBeenCalledWith(mockCustomers[0]);
   });
 });
