58b4986139f86603e5d9f2161386d3c1f5e820a8
Acubyte <sivut_mek@outlook.com>
2023-10-09 21:02:51 +0700

    adjust placeholder style in search bar

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -114,7 +114,7 @@ const Example = () => {
         <div className="flex justify-center items-center mt-4 gap-4">
           <input 
             id="search-customers"
-            className="block w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="block w-full placeholder:italic placeholder:pl-1 rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             placeholder="Search... ex. customer name, email, etc."
             autoComplete="off"
             value={text}
